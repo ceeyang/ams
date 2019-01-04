d6f861379d935876b5c25409f442ded98f422457
yangxichuan <yangxichuan2016@gmail.com>
2019-01-04 17:01:41 +0800

    xlsx read success

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,27 +1,36 @@
 <template lang="html">
     <el-container style="height: device-height; border: 1px solid #eee">
-
       <el-container>
         <el-header style="text-align: right; font-size: 12px">
-          <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-          <span>王小虎</span>
+            <el-upload
+                ref="upload"
+                action="/upload/"
+                :on-change="readExcel"
+                :show-file-list="false"
+                :auto-upload="false">
+                <el-button
+                    slot="trigger"
+                    icon="el-icon-upload"
+                    size="small"
+                    type="primary">
+                    上传文件
+                </el-button>
+            </el-upload>
+
+            <el-button
+                slot="trigger"
+                icon="el-icon-upload"
+                size="small"
+                type="primary">
+                导出文件
+            </el-button>
         </el-header>
 
         <el-main>
           <el-table :data="tableData">
-            <el-table-column prop="date" label="日期" width="140">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="120">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+            <el-table-column prop="date" label="日期" width="140"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
           </el-table>
         </el-main>
       </el-container>
@@ -29,6 +38,7 @@
 </template>
 
 <script>
+import XLSX from 'xlsx';
 export default {
   data() {
     const item = {
@@ -39,8 +49,41 @@ export default {
     return {
       tableData: Array(20).fill(item)
     }
+  },
+  mounted() {
+
+  },
+  methods: {
+    // 导出按钮点击事件
+    exportBtnDidClick() {
+      alert('exportBtnDidClick')
+    },
+
+    /// 读取 xlsx 文件
+    readExcel(file) {
+      const fileReader = new FileReader();
+      fileReader.onload = (ev) => {
+        try {
+            debugger
+          const data = ev.target.result;
+          const workbook = XLSX.read(data, {
+            type: 'binary'
+          });
+          console.log(workbook);
+          for (let sheet in workbook.Sheets) {
+            const sheetArray = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]); //获得以第一列为键名的sheet数组对象
+            console.log(sheet);
+            console.log(sheetArray);
+          }
+        } catch (e) {
+          this.$message.warning('文件类型不正确！');
+          return false;
+        }
+      };
+      fileReader.readAsBinaryString(file.raw);
+    }
   }
-};
+}
 </script>
 
 <style>
