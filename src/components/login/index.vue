e4f8f71e0367fcb6df1840b16c03bd1388040cd8
yangxichuan <yangxichuan2016@gmail.com>
2019-01-05 19:59:24 +0800

    modify login ui

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,12 +1,24 @@
 <template lang="html">
-    <div class="">
-        <span>欢迎来到联恒考勤管理系统</span>
+    <div class="login-page">
+        <!-- <img class="login-page-logo" src="../../assets/logo.png"> -->
+        <span class="login-page-welcome">{{welcome}}</span>
+        <div class="login-page-input-view">
+            <el-input placeholder="请输入您的账号" v-model="account" clearable></el-input>
+            <el-input placeholder="请输入您的密码" v-model="password" clearable></el-input>
+        </div>
     </div>
 </template>
 
 <script>
 import { mapState, mapActions} from 'vuex';
 export default {
+    data() {
+        return {
+            welcome: '欢迎来到联恒考勤管理系统',
+            account: '',
+            password: '',
+        }
+    },
     computed: {
         ...mapState([
             'loginStore',
